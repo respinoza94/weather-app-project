9f8c086f928276abc895b81d0e2b1349e6377309
respinoza94 <rebekaluvsyou94@gmail.com>
2020-12-03 18:19:48 -0800

    Updated JS

---
+++ script.js
@@ -0,0 +1,40 @@
+let now = new Date();
+let h1 = document.querySelector("h1");
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let day = days[now.getDay()];
+
+h1.innerHTML = `${day}, ${hours}:${minutes}`;
+
+function displayCurrentWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function cityInput(event) {
+  event.preventDefault();
+  let apiKey = "e2d5b31fe9b2eb6fc818690f86f1d2c6";
+  let city = document.querySelector("#user-city").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayCurrentWeather);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("click", cityInput);
