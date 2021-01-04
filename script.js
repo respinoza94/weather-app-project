a26c6cc6abe8b65413c6c0a0b0be15a1868fc41d
respinoza94 <rebekaluvsyou94@gmail.com>
2021-01-03 19:16:40 -0800

    Updated Celsius JS functionality

--- script.js
+++ script.js
@@ -57,13 +57,13 @@ function displayFahrenheitTemperature(event) {
   unitsElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
-function displayCelsiusTemperature(event) {
+function convertToCelsius(event) {
   event.preventDefault();
-  let unitsElement = document.querySelector("#temperature");
-  unitsElement.innnerHTML = Math.round(celsiusTemperature);
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
-
-let celsiusTemperature = null;
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("click", cityInput);
@@ -72,4 +72,6 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
+celsiusLink.addEventListener("click", convertToCelsius);
+
+let celsiusTemperature = null;
